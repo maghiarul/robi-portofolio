9c4529aa75179550a32c34af2ea885626b0ef982
maghiarul <panaite.cristian99@gmail.com>
2022-11-19 21:10:23 +0200

    dll

---
+++ components/About.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about-me">
+      <h1>About me</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel
+        sapien aliquam, mollis ligula ac, eleifend sapien. Praesent feugiat,
+        nisl id ornare dictum, urna odio luctus erat, non efficitur libero enim
+        in odio. Aliquam vitae sapien quam.
+      </p>
+    </div>
+  );
+}
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import About from '../components/About'
 
 export default function Home() {
   return (
     <div className="container">
+      <section id='one'>
       <div className="heading">
         <h1>Hi, my name is </h1>
         <h2>Roberto Iulian Donici</h2>
       </div>
+      <div className='arrow'>
+        <FontAwesomeIcon icon={faArrowDown} id="arrow-down"/>
+      </div>
+      </section>
+      <section id='two'>
+        <About />
+      </section>
+
     </div>
   )
 }
